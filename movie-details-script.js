28c2682dfaeca942d153f3692ef84016c56fdf44
Clyla <clyla_a_rafanan@dlsu.edu.ph>
2022-05-18 04:59:38 +0800

    adding booking

--- movie-details-script.js
+++ movie-details-script.js
@@ -11,12 +11,41 @@ function changeTab(evt, locName) {
   }
   document.getElementById(locName).style.display = "block";
   evt.currentTarget.className += " active";
+  changeLoc(locName)
 }
 
-function changeText(text) {
-  document.getElementById("change").innerHTML = text;
+function changeLoc(locName) {
+  var text;
+  switch (locName) {
+    case "loc1":
+      text = "Location 1"
+      break;
+
+    case "loc2":
+      text = "Location 2"
+      break;
+
+    case "loc3":
+      text = "Location 3"
+      break;
+
+    case "loc4":
+      text = "Location 4"
+      break;
+
+    case "loc5":
+      text = "Location 5"
+      break;
+  }
+  document.getElementById('book-loc').innerHTML = text;
+  document.getElementById('book-date').innerHTML = "[date]";
+  document.getElementById('book-time').innerHTML = "[time]";
 }
 
-function showDiv() {
-   document.getElementById('show').style.display = "block";
+function changeText(text, change_id) {
+  document.getElementById(change_id).innerHTML = text;
+}
+
+function showDiv(div_id) {
+   document.getElementById(div_id).style.display = "block";
 }
