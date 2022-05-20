0baaa35fa9e68025b825b0ce9d74a3db46292149
Clyla <clyla_a_rafanan@dlsu.edu.ph>
2022-05-20 20:11:04 +0800

    adding edit forms

--- manager-edit.js
+++ manager-edit.js
@@ -13,3 +13,20 @@ function submitDates() {
   document.getElementById("unDate").style.visibility = "visible";
   document.getElementById("time-editor").style.visibility = "visible";
 }
+
+function submitMovie() {
+  var title = $("#title").val();
+  $(".msg").append('You added the movie "' + title + '"!');
+}
+
+function addMov() {
+  document.getElementById("add").style.display = "block";
+  document.getElementById("startEdit").style.display = "none";
+  document.getElementById("return").style.display = "block";
+}
+
+function editCinema() {
+  document.getElementById("edit").style.display = "block";
+  document.getElementById("startEdit").style.display = "none";
+  document.getElementById("return").style.display = "block";
+}
