44d147522d51b7324209e5d96fb09c5b0c13a294
PandaBunBuns <87511092+PandaBunBuns@users.noreply.github.com>
2022-06-26 23:45:48 +0800

    added og files

---
+++ public/js/manager.js
@@ -0,0 +1,52 @@
+function submitCinema() {
+    document.getElementById("cinema-editor").style.visibility = "visible";
+  }
+  
+  function addDateInput() {
+    $("#appendDate").append("<input type='date' name='newDate' class='unDates' required><br>");
+  }
+  
+  function submitDates() {
+    document.getElementById("unDate").style.visibility = "visible";
+    document.getElementById("time-editor").style.visibility = "visible";
+  }
+  
+  function submitMovie() {
+    var title = $("#title").val();
+    $(".msg").append('You added the movie "' + title + '"!');
+  }
+  
+  function addMov() {
+    document.getElementById("add").style.display = "block";
+    document.getElementById("startEdit").style.display = "none";
+    document.getElementById("return").style.display = "block";
+  }
+  
+  function editCinema() {
+    document.getElementById("edit").style.display = "block";
+    document.getElementById("startEdit").style.display = "none";
+    document.getElementById("return").style.display = "block";
+  }
+  
+  var ogTitle;
+  
+  function openEdit() {
+    document.getElementById("startEdit").style.display = "none";
+    ogTitle = $("#movies-select2 option:selected").text();
+    $("#movie-title").append('Currently Editing <span style="color:#B5179E">' + ogTitle + '</span>');
+    document.getElementById("editMovie").style.visibility = "visible";
+    document.getElementById("return").style.display = "block";
+    document.getElementById("submitEdit").style.display = "block";
+  }
+  
+  function submitAll() {
+    var title = $("#upTitle").val();
+  
+    if (title.length == 0){
+      title = ogTitle;
+    }
+  
+    document.getElementById("editMovie").style.display = "none";
+    document.getElementById("submitEdit").style.display = "none";
+    $("#movie-title").text('You have succesfully edited ' + title);
+  }
