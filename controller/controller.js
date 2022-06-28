f0b53be0fc38d4a8c275f89ccd130a2bb8f0c67b
KatarinaLouise <lacson100@gmail.com>
2022-06-28 15:40:06 +0800

    made postRegistraton

--- controller/controller.js
+++ controller/controller.js
@@ -122,6 +122,48 @@ const controller = {
                }
            })
        },
+       postRegistration: (req, res)=>{ //edit registration page for the hbs to include the error
+            UserModel.findOne({'email' : req.body.email}, (err, user)=>{
+                if(user)
+                {
+                    res.render('registration', {error: "Email already exists"})
+                }
+            })
+            if(req.body.password1 == req.body.password2)//if password and confirm password are the same
+            {
+                let user = new UserModel({
+                    username : req.body.username,
+                    firstName : req.body.firstName,
+                    lastName : req.body.lastName,
+                    password : req.body.password
+                })
+                let card_deets = new CardModel({
+                    cardNum : req.body.cardNum,
+                    username : req.body.username,
+                    firstName : req.body.firstName,
+                    lastName : req.body.lastName,
+                    expiration : req.body.expiration,
+                    cardType : req.body.cardType,
+                    cvv : req.body.cvv,     
+                })
+
+                //save details to db, pero di ako sure here
+                user.save(function (err){
+                    if (err)
+                        res.render('registration',{error: "Error"})    
+                    else
+                        res.render('login')
+                })
+                card_deets.save(function (err)
+                {//not sure with this portion specifically
+                    if (err)
+                        res.render('registration',{error: "Error"})    
+                    else
+                        res.render('login')
+                }
+                )
+            }
+       },
        getAboutUs: (req, res)=>{
            if(req.cookies.user)
                req.session.user = req.cookies.user;
