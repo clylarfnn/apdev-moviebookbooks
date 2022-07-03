f8054fb4219906de0ce974210d6ebf5cfe45ac6e
PandaBunBuns <87511092+PandaBunBuns@users.noreply.github.com>
2022-07-03 20:39:43 +0800

    manager

--- controller/controller.js
+++ controller/controller.js
@@ -178,16 +178,28 @@ const controller = {
                else{
                    UserModel.findOne({'username': username}, (err, user)=>{
                         CardModel.findOne({'username': username}, (err, card)=>{
-                            db.findMany(BookingModel, {}, username, async function(result) {
-                                var length = result.length;
-                                var bookings = new Array(length);
-                                for(i=0; i<length; i++){
-                                    BookingModel.findOne({'_id':result[i]}, (err, book)=>{
-                                        console.log("book",book)
-                                        bookings[i] = book;
-                                    });
+                            db.findMany(BookingModel, {}, {'username': username}, async function(result) {
+                                var bookingHistory = [];
+                                var currentBooking = [];
+
+                                for(i=0; i<result.length; i++){
+                                    console.log(result[i]);
+                                    if(result[i].done){
+                                        bookingHistory.push(result[i]);
+                                    }
+                                    else{
+                                        currentBooking.push(result[i]);
+                                    }
                                 }
-                                res.render("userProfile", {user: user, card: card, booking: bookings});
+
+                                console
+
+                                res.render("userProfile", {
+                                    user: user,
+                                    card: card, 
+                                    bookingHistory: bookingHistory, 
+                                    currentBooking: currentBooking
+                                });
                             });  
                         });
                     });
